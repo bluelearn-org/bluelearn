c6ca0775e4de60b7b07013043f94bcf8e5e74d4b
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-06-28 02:51:32 +0100

    feat(app): update register form styling
    Signed-off-by: andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>

--- app/src/routes/register.tsx
+++ app/src/routes/register.tsx
@@ -5,10 +5,10 @@ export const Route = createFileRoute("/register")({ component: RouteComponent })
 
 function RouteComponent() {
   return (
-<div className="flex min-h-[calc(100svh_-_70px)] flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-  <div className="flex max-w-[1280px] flex-col gap-6">
-    <RegisterForm />
-  </div>
-</div>
+    <div className="flex min-h-[calc(100svh_-_70px)] flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <div className="flex max-w-[1280px] flex-col gap-6">
+        <RegisterForm />
+      </div>
+    </div>
   )
 }
