9d6641bf034f89b191f8f8521c381d27a11c1203
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-07-20 11:56:01 +0100

    feat: add initial variant details fields
    Signed-off-by: andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>

--- app/src/components/contribute/steps/VariantDetails.tsx
+++ app/src/components/contribute/steps/VariantDetails.tsx
@@ -1,15 +1,237 @@
+// import { useState } from "react";
+import type { Dispatch, SetStateAction } from "react";
+import type { GuideContribution } from "@/types/contributions";
+
 import { StepperActionHeader } from "@/components/contribute/StepperActionHeader";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+// import subjectsData from "@/data/subjects.json";
+// import guidesData from "@/data/guides.json";
+// import { Button } from "@/components/ui/button";
+// import { Combobox } from "@/components/ui/combobox";
 
 type PropTypes = {
   Stepper: any;
+  variantContData: GuideContribution;
+  setVariantContData: Dispatch<SetStateAction<GuideContribution>>;
 };
 
-export const VariantDetails = ({ Stepper }: PropTypes) => {
+export const VariantDetails = ({
+  Stepper,
+  variantContData,
+  setVariantContData,
+}: PropTypes) => {
+  // const [todoPrereq, setTodoPrereq] = useState<string>("");
+  // const [newSubject, setNewSubject] = useState<{
+  //   name: string;
+  //   summary: string;
+  // }>({
+  //   name: "",
+  //   summary: "",
+  // });
+
   return (
     <Stepper.Content step="variant-details">
       <StepperActionHeader title={"Variant Details"} Stepper={Stepper} />
 
-      <h2>Variant Guide Name</h2>
+      <FieldGroup>
+        <Field className="space-y-2">
+          <FieldLabel className="font-mono tracking-[0.08em] uppercase">
+            Title
+          </FieldLabel>
+
+          <Input
+            id="title"
+            type="text"
+            autoComplete="Title"
+            maxLength={50}
+            placeholder="Choose a title. (Maximum 50 characters)."
+            className="h-10 rounded-md"
+            required
+            value={variantContData.title}
+            onChange={(e) =>
+              setVariantContData((prev) => ({
+                ...prev,
+                title: e.target.value,
+              }))
+            }
+          />
+        </Field>
+
+        <Field className="space-y-2">
+          <FieldLabel className="font-mono tracking-[0.08em] uppercase">
+            Summary
+          </FieldLabel>
+
+          <textarea
+            className="h-32 w-full min-w-0 resize-none rounded-md border border-input bg-input/20 p-2 text-sm transition-colors outline-none file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-xs/relaxed file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-2 focus-visible:ring-ring/30 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-2 aria-invalid:ring-destructive/20 md:text-xs/relaxed dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40"
+            rows={4}
+            placeholder="Write a summary for your guide variant."
+            required
+            value={variantContData.summary}
+            onChange={(e) =>
+              setVariantContData((prev) => ({
+                ...prev,
+                summary: e.target.value,
+              }))
+            }
+          />
+        </Field>
+
+        {/* <Field className="space-y-2">
+          <FieldLabel className="font-mono tracking-[0.08em] uppercase">
+            Subjects
+          </FieldLabel>
+
+          <Combobox
+            multiple
+            items={subjectsData.map((s) => {
+              return {
+                value: s.slug,
+                label: s.name,
+                description: s.summary,
+              };
+            })}
+            value={guideContData.subjects}
+            onValueChange={(subjects) =>
+              setGuideContData((prev) => ({
+                ...prev,
+                subjects,
+              }))
+            }
+          />
+        </Field> */}
+        {/* <Field className="space-y-2">
+          <FieldLabel className="font-mono tracking-[0.08em] uppercase">
+            New Subjects
+          </FieldLabel>
+          <div className="flex items-center justify-between gap-4">
+            <Input
+              id="new-subject-name"
+              type="text"
+              maxLength={50}
+              placeholder="Enter subject name."
+              className="h-10 rounded-md"
+              value={newSubject.name}
+              onChange={(e) =>
+                setNewSubject((prev) => ({
+                  ...prev,
+                  name: e.target.value,
+                }))
+              }
+            />
+
+            <Input
+              id="new-subject-summary"
+              type="text"
+              maxLength={50}
+              placeholder="Enter summary of new subject."
+              className="h-10 rounded-md"
+              value={newSubject.summary}
+              onChange={(e) =>
+                setNewSubject((prev) => ({
+                  ...prev,
+                  summary: e.target.value,
+                }))
+              }
+            />
+            <Button
+              variant="ghost"
+              size="icon"
+              className="btn-sec h-10 w-24 rounded-md"
+              onClick={() => {
+                if (newSubject.name !== "" && newSubject.summary !== "") {
+                  const newSubs = [...guideContData.newSubjects, newSubject];
+                  setGuideContData((prev) => ({
+                    ...prev,
+                    newSubjects: newSubs,
+                  }));
+
+                  setNewSubject({ name: "", summary: "" });
+                }
+              }}
+            >
+              Add Subject
+            </Button>
+          </div>
+        </Field> */}
+
+        {/* <ul className="list-disc px-8 text-[11px] text-muted-foreground">
+          {variantContData.newSubjects.map((sub, index) => {
+            return (
+              <li key={index}>
+                {sub.name} - {sub.summary}
+              </li>
+            );
+          })}
+        </ul> */}
+
+        {/* <Field className="space-y-2">
+          <FieldLabel className="font-mono tracking-[0.08em] uppercase">
+            Prerequsite Guides
+          </FieldLabel>
+
+          <Combobox
+            multiple
+            items={guidesData.map((g) => {
+              return {
+                value: g.slug,
+                label: g.title,
+                description: g.summary,
+              };
+            })}
+            value={variantContData.prereqs}
+            onValueChange={(prereqs) =>
+              setVariantContData((prev) => ({
+                ...prev,
+                prereqs,
+              }))
+            }
+          />
+        </Field> */}
+
+        {/* <Field className="space-y-2">
+          <FieldLabel className="font-mono tracking-[0.08em] uppercase">
+            Todo Prerequsite Guides
+          </FieldLabel>
+
+          <div className="flex items-center justify-between gap-4">
+            <Input
+              id="todo-prereqs"
+              type="text"
+              maxLength={50}
+              placeholder="Enter title of missing prerequsite guide."
+              className="h-10 rounded-md"
+              value={todoPrereq}
+              onChange={(e) => setTodoPrereq(e.target.value)}
+            />
+            <Button
+              variant="ghost"
+              size="icon"
+              className="btn-sec h-10 w-24 rounded-md"
+              onClick={() => {
+                if (todoPrereq !== "") {
+                  const todos = [...guideContData.todoPrereqs, todoPrereq];
+                  setGuideContData((prev) => ({
+                    ...prev,
+                    todoPrereqs: todos,
+                  }));
+
+                  setTodoPrereq("");
+                }
+              }}
+            >
+              Add Guide
+            </Button>
+          </div>
+        </Field> */}
+        {/* <ul className="list-disc px-8 text-[11px] text-muted-foreground">
+          {variantContData.todoPrereqs.map((todo, index) => {
+            return <li key={index}>{todo}</li>;
+          })}
+        </ul> */}
+      </FieldGroup>
     </Stepper.Content>
   );
 };
