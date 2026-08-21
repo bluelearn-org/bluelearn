6a24dfe5d4aae984552a1db26c3a366d6e2bd245
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-08-22 00:47:40 +0100

    feat: add action button on verifier assignmement page
    Signed-off-by: andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>

---
+++ app/src/components/icons/UserRoundArrowLeft.tsx
@@ -0,0 +1,21 @@
+export const UserRoundArrowLeft = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-user-round-arrow-left-icon lucide-user-round-arrow-left"
+    >
+      <path d="m19 16-3 3" />
+      <path d="M2 21a8 8 0 0 1 12.664-6.5" />
+      <path d="M22 19h-6l3 3" />
+      <circle cx="10" cy="8" r="5" />
+    </svg>
+  );
+};
--- app/src/routes/dashboard/assignments.tsx
+++ app/src/routes/dashboard/assignments.tsx
@@ -5,15 +5,5 @@ export const Route = createFileRoute("/dashboard/assignments")({
 });
 
 function RouteComponent() {
-  return (
-    <div className="space-y-5">
-      <header className="space-y-1.5 border-b border-border pb-5">
-        <h1 className="font-mono text-[14px] tracking-[0.08em] text-muted-foreground uppercase">
-          Manage Verifier Assignments
-        </h1>
-      </header>
-
-      <section className="space-y-3"></section>
-    </div>
-  );
+  return <div>Hello "/dashboard/assignments"!</div>;
 }
